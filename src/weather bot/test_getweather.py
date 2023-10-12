0579e85c35d0571dd34824a7b16201d6eaa0d825
vovasobol1 <dengyrostov@gmail.com>
2023-10-12 15:36:59 +0300

    исправлены тесты

--- src/weather bot/test_getweather.py
+++ src/weather bot/test_getweather.py
@@ -4,38 +4,27 @@ from main import getWeather
 from config import weatherApiToken
 
 class TestGetWeather(unittest.TestCase):
-
-    @patch('requests.get')
-    def test_get_weather_successful(self, mock_get):
-        # Мокируем запрос и настраиваем его ответ
-        mock_response = mock_get.return_value
-        mock_response.json.return_value = {
-            'name': 'City',
-            'main': {'temp': 20, 'humidity': 50},
-            'wind': {'speed': 5},
-            'sys': {'sunrise': 1634000000, 'sunset': 1634040000}
-        }
-
-        result = getWeather('City', weatherApiToken)
-        expected_result = (
-            "погода в City \n"
-            "температура воздуха : 20°\n"
-            "влажность :50% \n"
-            "скорость ветра : 5м/с \n"
-            "давление :1013мм \n"  
-            "рассвет: 03:46:40 \n"
-            "закат: 18:33:20 \n"
-        )
-        self.assertEqual(result, expected_result)
+    # Этот класс представляет набор юнит-тестов для функции getWeather.
 
     @patch('requests.get')
     def test_get_weather_exception(self, mock_get):
-        # Мокируем запрос, но настраиваем его на выброс исключения
+        # Этот метод тестирует обработку исключения в функции getWeather при неудачном запросе к апи
+
+        # Мокируем запрос, чтобы изолировать функцию от фактического HTTP-запроса
+        # mock_get - объект-замена для requests.get, который будет имитировать его вызов
         mock_get.side_effect = Exception("Test exception")
 
-        result = getWeather('InvalidCity', 'your_token')
+        # Вызываем функцию getWeather с неверным названием города
+        result = getWeather('invalidcity', weatherApiToken)
+
+        # Ожидаемый результат: строка с сообщением об ошибке.
         expected_result = 'Не удалось получить информацию о погоде. Пожалуйста, проверьте название города.'
+
+        # Сравниваем фактический результат с ожидаемым, чтобы убедиться, что функция правильно обрабатывает ошибку.
         self.assertEqual(result, expected_result)
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
