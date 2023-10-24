533a6fc0f3aa42d4c7d6da7219ddd3b439029220
vovasobol1 <dengyrostov@gmail.com>
2023-10-25 01:23:44 +0300

    реализованы фунции encrypt_caesar и decrypt_caesar , написал юнит тесты для них

---
+++ src/lab2/test_caesar.py
@@ -0,0 +1,25 @@
+import unittest
+from caesar import encrypt_caesar, decrypt_caesar
+class TestCaesarCipher(unittest.TestCase):
+    def test_encrypt_caesar(self):
+        # Проверяем зашифрованный текст с сдвигом 3
+        self.assertEqual(encrypt_caesar("PYTHON"), "SBWKRQ")
+        self.assertEqual(encrypt_caesar("python"), "sbwkrq")
+        self.assertEqual(encrypt_caesar("Python3.6"), "Sbwkrq3.6")
+        self.assertEqual(encrypt_caesar(""), "")  # Пустая строка должна оставаться пустой
+
+        # Проверяем, что исключение вызывается при некорректном типе данных
+        with self.assertRaises(ValueError):
+            encrypt_caesar(123 , 5 )  # Попытка зашифровать целое число
+    def test_decrypt_caesar(self):
+        # Проверяем расшифрованный текст с сдвигом 3
+        self.assertEqual(decrypt_caesar("SBWKRQ"), "PYTHON")
+        self.assertEqual(decrypt_caesar("sbwkrq"), "python")
+        self.assertEqual(decrypt_caesar("Sbwkrq3.6"), "Python3.6")
+        self.assertEqual(decrypt_caesar(""), "")  # Пустая строка должна оставаться пустой
+
+        # Проверяем, что исключение вызывается при некорректном типе данных
+        with self.assertRaises(ValueError):
+            encrypt_caesar(123 , 5 )  # Попытка зашифровать целое число
+if __name__ == '__main__':
+    unittest.main()
